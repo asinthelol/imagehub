c59dac172b28bdac57fd2fd5d56ef20eae69e6f4
Kevin <asinthexd@gmail.com>
2025-01-31 17:58:27 -0500

    Added content to #container on home page
    Same as title.
    - Made color-primary-underline to make the transparent property only used for buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <main id={styles.container}>
-      <div></div>
+      <section id={styles.hero}>
+        <div id={styles["hero-content"]}>
+          <h1>
+            Your favorites,<br />
+            all in <span>one place</span>.
+          </h1>
+          <p>Discover your next favorite artwork<br />right here.</p>
+        </div>
+        <div id={styles["hero-image-container"]}>
+          <Image
+            id={styles["hero-image"]}
+            src="/hero-image.webp"
+            fill
+            alt="hero image" />
+        </div>
+      </section>
     </main>
   );
 }
