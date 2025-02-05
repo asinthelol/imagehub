fc46f05c920d6fcf9960eabe58b0f07f7c532a91
Kevin <asinthexd@gmail.com>
2025-02-04 22:39:50 -0500

    Update gitignore
    Same as title.

--- backend/ImageHubAPI/ImageHubAPI/obj/Debug/net8.0/ImageHubAPI.AssemblyInfo.cs
+++ backend/ImageHubAPI/ImageHubAPI/obj/Debug/net8.0/ImageHubAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImageHubAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9da45e74f7e84358f79b633fd79e13c2ce299c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69307c18df6ea741a693e002af4cf0627bccea8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImageHubAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImageHubAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
